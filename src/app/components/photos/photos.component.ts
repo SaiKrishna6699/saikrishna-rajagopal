0ea299debe5f91d379765fa64cbd1db707869a61
saikrishna1806 <saikrishna.rajagopal@relanto.ai>
2024-08-13 23:43:31 +0530

    Made changes in Scroll Bar and Video Rendering across other browsers

--- src/app/components/photos/photos.component.ts
+++ src/app/components/photos/photos.component.ts
@@ -1,12 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-photos',
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.scss']
 })
-export class PhotosComponent {
+export class PhotosComponent implements OnInit{
 
   @Input() mediaItems: any = [];
   @Input() title: String = '';
+
+  safeVideoUrl!: SafeResourceUrl;
+  safeVideoUrl1!: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer){
+    
+  }
+
+  ngOnInit(): void {
+    this.safeVideoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.mediaItems[5].url);
+    this.safeVideoUrl1 = this.sanitizer.bypassSecurityTrustResourceUrl(this.mediaItems[6].url);
+  }
 }
